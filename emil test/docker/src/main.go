af7cb2d2e7cd7a3ef910f2ea282d3640167b0329
Emil <lime.posklilja@hotmail.com>
2020-09-10 15:16:41 +0200

    working lab

--- emil test/docker/src/main.go
+++ emil test/docker/src/main.go
@@ -1,30 +1,99 @@
 package main
 
 import (
+	"bytes"
+	"encoding/gob"
 	"fmt"
-	"net/http"
-	"os"
-	"strings"
+	"io"
+	"log"
+	"net"
+	"time"
 )
 
-func main() {
-	var PORT string
-	if PORT = os.Getenv("PORT"); PORT == "" {
-		PORT = "3001"
-	}
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		var message string = r.URL.Path[1:]
-		message = strings.ToLower(message)
-		if message == "hello" {
-			fmt.Fprintf(w, "world!")
-		} else {
-			fmt.Fprintf(w, "Go to /Hello")
-		}
-		//fmt.Fprintf(w, "Hello World from path: %s\n", r.URL.Path)
-	})
-	http.ListenAndServe(":"+PORT, nil)
+type Message struct {
+	ID   string
+	Data string
 }
 
-func waitForInput() {
-	//var test string
+func logerr(err error) bool {
+	if err != nil {
+		if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
+			log.Println("read timeout:", err)
+		} else if err == io.EOF {
+		} else {
+			log.Println("read error:", err)
+		}
+		return true
+	}
+	return false
+}
+
+func server() {
+	ln, err := net.Listen("tcp", ":3000")
+	if err != nil {
+		// handle error
+	}
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			// handle error
+		}
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	timeoutDuration := 2 * time.Second
+	//fmt.Println("Launching server...")
+	conn.SetReadDeadline(time.Now().Add(timeoutDuration))
+
+	remoteAddr := conn.RemoteAddr().String()
+	fmt.Println("Client connected from " + remoteAddr)
+
+	read(conn)
+	resp(conn)
+}
+
+func read(conn net.Conn) {
+	// create a temp buffer
+	tmp := make([]byte, 500)
+
+	// loop through the connection to read incoming connections. If you're doing by
+	// directional, you might want to make this into a seperate go routine
+	for {
+		_, err := conn.Read(tmp)
+		if logerr(err) {
+			break
+		}
+
+		// convert bytes into Buffer (which implements io.Reader/io.Writer)
+		tmpbuff := bytes.NewBuffer(tmp)
+		tmpstruct := new(Message)
+
+		// creates a decoder object
+		gobobj := gob.NewDecoder(tmpbuff)
+		// decodes buffer and unmarshals it into a Message struct
+		gobobj.Decode(tmpstruct)
+
+		// lets print out!
+		fmt.Println(tmpstruct.ID)
+		return
+	}
+}
+
+func resp(conn net.Conn) {
+	msg := Message{ID: "PING", Data: "I responded"}
+	bin_buf := new(bytes.Buffer)
+
+	// create a encoder object
+	gobobje := gob.NewEncoder(bin_buf)
+	// encode buffer and marshal it into a gob object
+	gobobje.Encode(msg)
+
+	conn.Write(bin_buf.Bytes())
+	conn.Close()
+}
+
+func main() {
+	server()
 }
