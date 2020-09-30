a0ac3bea5cc641b6dad5ef8e6ad1498b1f84b1d4
Emil <lime.posklilja@hotmail.com>
2020-10-01 01:03:02 +0200

    can identify 1st

--- emil test/init/src/main/main.go
+++ emil test/init/src/main/main.go
@@ -1,27 +1,33 @@
 package main
 
 import (
-	"net"
 	"fmt"
+	"net"
 	"time"
 )
 
-const MAIN_HOSTNAME = "docker_testrun_1"
+const MAIN_HOSTNAME = "init_testrun_1"
 
 func main() {
-	inits()
+	go inits()
 	server("3002")
 }
 
 func inits() {
+	time.Sleep(3 * time.Second)
 	ips, err := net.LookupIP(MAIN_HOSTNAME)
 	if err != nil {
 		return
 	}
-	if ips[0].String() == "127.0.0.1" {
+	localIPs, _ := net.InterfaceAddrs()
+	localIP := localIPs[1].String()[:(len(localIPs[1].String()) - 3)]
+	time.Sleep(3 * time.Second)
+
+	if ips[0].String() == localIP {
+		fmt.Println("Yah")
 		return
 	}
+	fmt.Println("Nah")
 	time.Sleep(5 * time.Second)
 	client(MAIN_HOSTNAME, "3002", Message{CMD: "PING"})
-	fmt.Println(ips)
 }
